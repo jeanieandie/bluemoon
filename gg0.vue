6737809ab5e8738b9dc57f9a2128d4f4f68bdb69
jeanieandie <77466070+jeanieandie@users.noreply.github.com>
2021-08-25 11:16:55 -0700

    trying to put outlines here in public

---
+++ gg0.vue
@@ -0,0 +1,206 @@
+<template lang="pug">
+pre(style='background-color: black; color: white') this is bluemoon/src/views/gg0.vue :: here i would put links to index and meta info such as 2030 goals and how to get from here to there
+div(class="home")(style='background-color: #000')
+  h1 datawires; proprietary and "not mine" to give away
+  h2 bluemoon; puplicly stored on github @ https://github.com/jeanieandie/bluemoon
+  table
+    tr
+      td(style='background-color: black;')
+        img(alt="001.png" src="../assets/001.png")
+      td(style='background-color: grey;')
+        img(alt="001.png" src="../assets/001.png")
+      td(style='background-color: white;')
+        img(alt="001.png" src="../assets/001.png")
+      td(style='background-color: pink;')
+        img(alt="001.png" src="../assets/001.png")
+      td(style='background-color: hotpink;')
+        img(alt="001.png" src="../assets/001.png")
+      td(style='background-color: lime;')
+        img(alt="001.png" src="../assets/001.png")
+      td(style='background-color: indigo;')
+        img(alt="001.png" src="../assets/001.png")
+
+table(style='width: 1820px; padding: 25px; box-sizing: border-box; background-color: #7f7f7f;')
+  tr
+    td(style='background-color: #888; color: hotpink; font-size: 13px;')
+      router-link(to="/gg0" style='background-color: #888; color: #777; font-size: 13px;') 人 <br>
+        i.material-icons(style='color: #hotpink;') swap_horiz
+      h6 swap_horiz
+    td
+      p think carousel.
+  tr
+    td(style='background-color: #777; color: hotpink; font-size: 13px;')
+      router-link(to="/gg0" style='background-color: #777; color: #888; font-size: 13px;') 愛 <br>
+        i.material-icons(style='color: #hotpink;') tonality
+      h6 tonality
+    td
+      p tonality is used to describe music, colors, speach, etc
+  tr
+    td(style='background-color: #a020f0')
+      router-link(to="/gg0" style='color: #bdf500; font-size: 33px;') ⑯
+        br
+        i.material-icons(style='color: #bdf500; font-size: 33px;') art_track
+      h6 art_track
+    td(style='background-color: #bdf500; color: #a020f0; font-size: 33px;')
+      p use art to track habits. art is where you habit-@.
+  tr
+    td(style='background-color: #000')
+      router-link(to="/gg0" style='color: #fff; font-size: 33px;') ⑮
+        br
+        i.material-icons(style='color: #fff; font-size: 33px;') ac_unit
+      h6 ac_unit
+    td(style='background-color: #fff; color: #000; font-size: 33px;')
+      p oxygen matters
+  tr
+    td(style='background-color: #eee')
+      router-link(to="/gg0" style='color: #000; font-size: 33px;') ⑭
+        br
+        i.material-icons(style='color: #000; font-size: 33px;') accessible
+      h6 accessible
+    td
+      p move it or lose it. goal: take garbage out using chair
+  tr
+    td(style='background-color: #ddd')
+      router-link(to="/gg0" style='color: pink; font-size: 33px;') ⑬
+        br
+        i.material-icons(style='color: pink; font-size: 33px;') account_balance
+      h6 account_balance
+    td
+      p Nazarenes vote with $$$. Time for supplication for the et al
+  tr
+    td(style='background-color: #ccc')
+      router-link(to="/gg0" style='color: brown; font-size: 33px;') ⑫
+        br
+        i.material-icons(style='color: brown; font-size: 33px;') adjust
+      h6 adjust
+    td
+      p i think this is a great icon for a place to end and begin again.
+  tr
+    td(style='background-color: #bbb')
+      router-link(to="/gg0" style='color: cyan; font-size: 33px;') ⑪
+        br
+        i.material-icons(style='color: cyan; font-size: 33px;') airport_shuttle
+      h6 airport_shuttle
+    td
+      p transportation matters. ride to care != safe ride
+  tr
+    td(style='background-color: #aaa')
+      router-link(to="/gg0" style='color: hotpink; font-size: 33px;') ⑩
+        br
+        i.material-icons(style='color: hotpink; font-size: 33px;') archive
+      h6 archive
+    td
+      p what libraries are we feeding both publicly and privately?
+  tr
+    td(style='background-color: #999; color: hotpink;')
+      router-link(to="/gg0" style='color: dimgrey; font-size: 33px;') ⑨
+        br
+        i.material-icons(style='color: dimgrey; font-size: 33px;') apps
+      h6 apps
+    td
+      p let's use technology to improve our lives, especially for medical matters
+  tr
+    td(style='background-color: #888')
+      router-link(to="/gg0" style='color: black; font-size: 33px;') ⑧
+        br
+        i.material-icons(style='color: black; font-size: 33px;') album
+      h6 album
+    td
+      p what does color sound like? Veronica's hex is #a020f0 long before I had a say in it. What does album #a020f0 sound like?
+  tr
+    td(style='background-color: #777')
+      router-link(to="/gg0" style='color: white; font-size: 33px;') ⑦
+        br
+        i.material-icons(style='color: white; font-size: 33px;') all_inclusive
+      h6 all_inclusive
+    td
+      p we are not all_inclusive until Aunt Michelle has said so.
+  tr
+    td(style='background-color: #666;')
+      router-link(to="/gg0" style='color: darkseagreen; font-size: 33px;') ⑥
+        br
+        i.material-icons(style='color: darkseagreen; font-size: 33px;') assistant_photo
+      h6 assistant_photo
+    td
+      p what flag do I fly?
+  tr
+    td(style='background-color: #555')
+      router-link(to="/gg0" style='color: powderblue; font-size: 33px;') ⑤
+        br
+        i.material-icons(style='color: powderblue; font-size: 33px;') widgets
+      h6 widgets
+    td
+      p datawires should be in every project we publish.
+  tr
+    td(style='background-color: #444')
+      router-link(to="/gg4" style='color: thistle; font-size: 33px;') ④
+        br
+        i.material-icons(style='color: thistle; font-size: 33px;') change_history
+      h6 change_history
+    td
+      p ABD∆
+  tr
+    td(style='background-color: #333')
+      router-link(to="/gg3" style='color: indianred; font-size: 33px;') ↋
+        br
+        i.material-icons(style='color: indianred; font-size: 33px;') kitchen
+      h6 kitchen
+    td(style='background-color: indianred; color: #333;')
+      p this is where I need to focus ○ ◌ ◍ ● ◐ ◑ ◒ ◓ ◔ ◕ ◖ ◗ ◙ ◚ ◛ ◠ ◡ ◯ ◴ ◵ ◶ ◷ ⚆ ⚇ ⚈ ⚉ ⚪ ⚫ ⚬ ✪ ❂ ❍ ⟟ ⦲ ⦵ ⦶ ⦷ ⦸ ⦹ ⦺ ⦻ ⦾ ⦿ ⧂ ⧃ ⧇ ⧲ ⧳ ⨀ ⨁ ⨂ ⫱ ⬤ ⭕ ○ ◌ ⬬ ⬭ ⬮ ⬯ ☷ °
+  tr
+    td(style='background-color: #222')
+      router-link(to="/gg2" style='color: orange; font-size: 33px;') ↊
+        br
+        i.material-icons(style='color: orange; font-size: 33px;') keyboard_capslock
+      h6 keyboard_capslock
+    td(style='background-color: orange; color: #222;')
+      p caps letters are used for Proper nouns.
+  tr
+    td(style='background-color: #111')
+      router-link(to="/gg1" style='color: yellow; font-size: 33px;') ①
+        br
+        i.material-icons(style='color: yellow; font-size: 33px;') lens
+      h6 lens
+    td(style='background-color: yellow; color: #111')
+      p sdf
+  tr
+    td(style='background-color: #000')
+      router-link(to="/gg0" style='color: #fff; font-size: 33px;') ⓪
+        br
+        i.material-icons(style='font-size: 33px;') home
+      h6 home
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+    td
+      h1 df
+
+
+pre(style='background-color: black; color: white') this is bluemoon/src/views/gg0.vue :: here i would put links to index and meta info such as 2030 goals and how to get from here to there
+</template>
+
+<script lang="coffee">
+# @ is an alias to /src
+
+export default
+  name: 'gg0'
+</script>
